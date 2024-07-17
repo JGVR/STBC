2a719737cc7d044e389849fa87b095bf891b7645
JGVR <juan@galileolegal.ai>
2024-07-17 13:42:23 -0400

    made the delete and update methods of the handler class to be concrete methods instead of abstract methods

--- stbcApi/app/services/church_handler.py
+++ stbcApi/app/services/church_handler.py
@@ -1,7 +1,7 @@
 from .handler import Handler
 from ..models.church import Church
 from pymongo.collection import Collection
-from typing import Dict
+from typing import Dict, Any
 from ..utils.type import Type
 from datetime import datetime
 
@@ -16,3 +16,15 @@ class ChurchHandler(Handler):
         }
         data.update(church.model_dump(by_alias=True))
         return {"_id": collection.insert_one(data).inserted_id}
+    
+    def find(self, filter: Dict[str, Any], collection: Collection) -> Church:
+        if not isinstance(filter, dict):
+            raise ValueError(f"Input data expected to be a dictionary")
+        
+        data = collection.find_one(filter)
+        if data is not None:
+            return Church(
+                church_id = data["churchId"],
+                name = data["name"]
+            )
+        return None
--- stbcApi/app/services/handler.py
+++ stbcApi/app/services/handler.py
@@ -1,4 +1,6 @@
 from abc import ABC, abstractmethod
+from typing import Dict, Any
+from pymongo.collection import Collection
 
 class Handler(ABC):
     @abstractmethod
@@ -9,10 +11,14 @@ class Handler(ABC):
     def find() -> None:
         pass
 
-    @abstractmethod
-    def update() -> None:
-        pass
+    def update(self, filter: Dict[str, Any], new_data: Dict[str, Any], collection: Collection) -> Dict[str, int]:
+        if not isinstance(filter, dict) or not isinstance(new_data, dict):
+            raise ValueError(f"The filter and new data input parameters must be a dictionary")
+        result = collection.update_one(filter, {"$set": new_data})
+        return {"count": result.modified_count}
 
-    @abstractmethod
-    def delete() -> None:
-        pass
+    def delete(self, filter: Dict[str, Any], collection: Collection) -> Dict[str, int]:
+        if not isinstance(filter, dict):
+            raise ValueError(f"The filter input parameter must be a dictionary")
+        
+        return {"count": collection.delete_one(filter).deleted_count}
