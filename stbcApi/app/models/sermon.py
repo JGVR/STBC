c2422d0a011d19e1388e313bd1209c95a01821e3
JGVR <juangabrielvasquez11@gmail.com>
2025-03-10 22:22:29 -0400

    added sermon and topic model classes

--- stbcApi/app/models/sermon.py
+++ stbcApi/app/models/sermon.py
@@ -1,11 +1,17 @@
 from pydantic import BaseModel, Field, ConfigDict
+from topic import Topic
+from datetime import datetime
+from typing import List
 
-class Church(BaseModel):
+class Service(BaseModel):
     # > makes fields immutable after instantiation
     # > instantiation can be made by using property name or alias name
     # > strips white spaces from all str fields
     # > forbids any new properties from been added to the object upon instantiation
     model_config = ConfigDict(frozen=True, populate_by_name=True, str_strip_whitespace=True, extra='forbid')
 
-    church_id: int = Field(gt=0, alias="churchId")
-    name: str = Field(max_length=250)
+    title: str = Field(max_length=300)
+    url: str
+    speaker_id: int = Field(gt=0, alias="speakerId")
+    date: datetime
+    topics: List[Topic]
